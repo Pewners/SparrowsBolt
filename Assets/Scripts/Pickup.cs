63093d85b38030b0d5ad053349eb04e26b69a650
Pewners <jfadikpe2021@elam.co.uk>
2022-02-11 01:45:42 +0000

    idk lol

--- Assets/Scripts/Pickup.cs
+++ Assets/Scripts/Pickup.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Pickup : MonoBehaviour
 {
+    private int score;
     private void OnTriggerEnter(Collider other)
     {
         //insert code that adds to counter
@@ -13,6 +16,9 @@ public class Pickup : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (score > 3)
+        {
+            SceneManager.LoadScene(4);
+        }
     }
 }
